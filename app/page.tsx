fd22987e952a98cd9e42244bf4a3d4ceb082a0d3
Oppong Meshack <oppongmeshack54@gmail.com>
2026-09-22 05:40:10 +0000

    Refactor global styles and add Navbar and Hero components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Niah's Bites</h1>
+      <h1 className="text-primary font-display text-4xl">Niah's Bites</h1>
     </main>
   );
 }
